c6b6f65947d59c3e6103a346ee4088792f4e615a
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-22 12:05:16 +0900

    invalid indexって何ですか

--- src/NeiroNetwork/ExperimentalFeatures/item/ItemIds.php
+++ src/NeiroNetwork/ExperimentalFeatures/item/ItemIds.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 namespace NeiroNetwork\ExperimentalFeatures\item;
 
 final class ItemIds{
-	public const RAW_IRON = -0x8000;
-	public const RAW_GOLD = -0x8000 + 1;
+	public const RAW_IRON = 600;
+	public const RAW_GOLD = 601;
 }
