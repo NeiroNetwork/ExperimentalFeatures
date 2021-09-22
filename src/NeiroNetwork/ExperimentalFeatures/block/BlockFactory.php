a836f2daa44ca5dfe82870891ae2f844ed35728e
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-22 12:09:19 +0900

    オーバーライドし忘れ

--- src/NeiroNetwork/ExperimentalFeatures/block/BlockFactory.php
+++ src/NeiroNetwork/ExperimentalFeatures/block/BlockFactory.php
@@ -14,7 +14,7 @@ class BlockFactory{
 
 	public static function init() : void{
 		$factory = \pocketmine\block\BlockFactory::getInstance();
-		$factory->register(new IronOre(new BlockIdentifier(BlockLegacyIds::IRON_ORE, 0), "Iron Ore", new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::STONE()->getHarvestLevel())));
-		$factory->register(new GoldOre(new BlockIdentifier(BlockLegacyIds::GOLD_ORE, 0), "Gold Ore", new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::IRON()->getHarvestLevel())));
+		$factory->register(new IronOre(new BlockIdentifier(BlockLegacyIds::IRON_ORE, 0), "Iron Ore", new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::STONE()->getHarvestLevel())), true);
+		$factory->register(new GoldOre(new BlockIdentifier(BlockLegacyIds::GOLD_ORE, 0), "Gold Ore", new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::IRON()->getHarvestLevel())), true);
 	}
 }
