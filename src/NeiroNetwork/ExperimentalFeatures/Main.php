19c4c072500afc04cd4eaf4e5e29d6641eb441ec
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-24 00:07:37 +0900

    MainからItemFactoryへ移動

--- src/NeiroNetwork/ExperimentalFeatures/Main.php
+++ src/NeiroNetwork/ExperimentalFeatures/Main.php
@@ -16,23 +16,8 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase{
 
 	protected function onEnable() : void{
-		// Register item instance
 		ExperimentalItemFactory::init();
-
-		// Add strings to parser
-		$parser = StringToItemParser::getInstance();
-		$parser->register("raw_iron", fn() => ExperimentalItems::RAW_IRON());
-		$parser->register("raw_gold", fn() => ExperimentalItems::RAW_GOLD());
-
-		// Register (override) block instance
 		ExperimentalBlockFactory::init();
-
-		// Hack network-item-translator
-		ItemTranslatorHack::prepare();
-		ItemTranslatorHack::hack(Ids::RAW_IRON, Ids::NET_RAW_IRON);
-		ItemTranslatorHack::hack(Ids::RAW_GOLD, Ids::NET_RAW_GOLD);
-
-		// Add recipes
 		CraftingRecipeInitializer::init();
 	}
 }
--- src/NeiroNetwork/ExperimentalFeatures/item/ExperimentalItemFactory.php
+++ src/NeiroNetwork/ExperimentalFeatures/item/ExperimentalItemFactory.php
@@ -4,15 +4,35 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures\item;
 
+use NeiroNetwork\ExperimentalFeatures\hack\ItemTranslatorHack;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIdentifier;
+use pocketmine\item\StringToItemParser;
 
 class ExperimentalItemFactory{
 
 	public static function init() : void{
+		self::registerItems();
+		self::registerItemStrings();
+		self::hackItemTranslator();
+	}
+
+	private static function registerItems() : void{
 		$factory = ItemFactory::getInstance();
 		$factory->register(new Item(new ItemIdentifier(ExperimentalItemIds::RAW_IRON, 0), "Raw Iron"));
 		$factory->register(new Item(new ItemIdentifier(ExperimentalItemIds::RAW_GOLD, 0), "Raw Gold"));
+	}
+
+	private static function registerItemStrings() : void{
+		$parser = StringToItemParser::getInstance();
+		$parser->register("raw_iron", fn() => ExperimentalItems::RAW_IRON());
+		$parser->register("raw_gold", fn() => ExperimentalItems::RAW_GOLD());
+	}
+
+	private static function hackItemTranslator() : void{
+		ItemTranslatorHack::prepare();
+		ItemTranslatorHack::hack(ExperimentalItemIds::RAW_IRON, ExperimentalItemIds::NET_RAW_IRON);
+		ItemTranslatorHack::hack(ExperimentalItemIds::RAW_GOLD, ExperimentalItemIds::NET_RAW_GOLD);
 	}
 }
