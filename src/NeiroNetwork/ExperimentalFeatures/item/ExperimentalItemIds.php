0550e6675643d1bd7e4d4f573cbed5d8d172e536
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-25 13:50:03 +0900

    IDを調べました

--- src/NeiroNetwork/ExperimentalFeatures/item/ExperimentalItemIds.php
+++ src/NeiroNetwork/ExperimentalFeatures/item/ExperimentalItemIds.php
@@ -9,4 +9,6 @@ class ExperimentalItemIds{
 	public const RAW_IRON = 600;
 	public const RAW_GOLD = 601;
 	public const GLOW_INK_SAC = 602;
+	public const RAW_IRON_BLOCK = 603;
+	public const RAW_GOLD_BLOCK = 604;
 }
--- src/NeiroNetwork/ExperimentalFeatures/item/NetworkItemIds.php
+++ src/NeiroNetwork/ExperimentalFeatures/item/NetworkItemIds.php
@@ -9,4 +9,6 @@ class NetworkItemIds{
 	public const RAW_IRON = 505;
 	public const RAW_GOLD = 506;
 	public const GLOW_INK_SAC = 503;
+	public const RAW_IRON_BLOCK = -451;
+	public const RAW_GOLD_BLOCK = -453;
 }
