943e412e54b232641e238f9977c6fedcecdf6184
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-25 14:46:01 +0900

    インポートのクリーンアップ

--- src/NeiroNetwork/ExperimentalFeatures/Main.php
+++ src/NeiroNetwork/ExperimentalFeatures/Main.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace NeiroNetwork\ExperimentalFeatures;
 
 use NeiroNetwork\ExperimentalFeatures\block\ExperimentalBlockFactory;
-use NeiroNetwork\ExperimentalFeatures\block\ExperimentalBlocks;
 use NeiroNetwork\ExperimentalFeatures\block\tile\ExperimentalTileFactory;
 use NeiroNetwork\ExperimentalFeatures\crafting\CraftingRecipeInitializer;
-use NeiroNetwork\ExperimentalFeatures\hack\ItemTranslatorHack;
 use NeiroNetwork\ExperimentalFeatures\item\ExperimentalItemFactory;
-use NeiroNetwork\ExperimentalFeatures\item\ExperimentalItemIds as Ids;
-use NeiroNetwork\ExperimentalFeatures\item\ExperimentalItems;
-use pocketmine\item\StringToItemParser;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
