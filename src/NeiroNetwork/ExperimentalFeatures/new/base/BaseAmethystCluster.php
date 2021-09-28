2d388a7730ebad53d060e6652f2edae11733bdc3
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-28 23:48:57 +0900

    Revert "アメジストの塊はピッケルが適性ツールだけど、手でも壊せる"
    This reverts commit e7d6ddde1a40ce3dd71ee2b999c9e1c82297d549.

--- src/NeiroNetwork/ExperimentalFeatures/new/base/BaseAmethystCluster.php
+++ src/NeiroNetwork/ExperimentalFeatures/new/base/BaseAmethystCluster.php
@@ -7,11 +7,12 @@ namespace NeiroNetwork\ExperimentalFeatures\new\base;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
 use pocketmine\block\BlockToolType;
+use pocketmine\item\ToolTier;
 
 abstract class BaseAmethystCluster extends AnyFacingBlock{
 
 	public function __construct(BlockIdentifier $idInfo, string $name){
-		parent::__construct($idInfo, $name, new BlockBreakInfo(1.5, BlockToolType::PICKAXE));
+		parent::__construct($idInfo, $name, new BlockBreakInfo(1.5, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel()));
 	}
 
 	public function isAffectedBySilkTouch() : bool{
