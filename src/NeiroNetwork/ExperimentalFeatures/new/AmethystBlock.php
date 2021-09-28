54c92700b183620aeb931053ff19df2173a32dca
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-29 00:01:27 +0900

    追加: アメジストブロックのレシピ

--- src/NeiroNetwork/ExperimentalFeatures/new/AmethystBlock.php
+++ src/NeiroNetwork/ExperimentalFeatures/new/AmethystBlock.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures\new;
 
+use NeiroNetwork\ExperimentalFeatures\new\interface\HasRecipe;
 use NeiroNetwork\ExperimentalFeatures\new\interface\IBlock;
+use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalBlocks;
+use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalItems;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
 use pocketmine\block\Opaque;
+use pocketmine\crafting\ShapedRecipe;
 use pocketmine\item\ToolTier;
 
-class AmethystBlock extends Feature implements IBlock{
+class AmethystBlock extends Feature implements IBlock, HasRecipe{
 
 	public function networkId() : int{
 		return -327;
@@ -26,5 +30,9 @@ class AmethystBlock extends Feature implements IBlock{
 			$this->blockId(), "Amethyst Block",
 			new BlockBreakInfo(1.5, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel())
 		);
+	}
+
+	public function recipe() : array{
+		return [new ShapedRecipe(["AA", "AA"], ["A" => ExperimentalItems::AMETHYST_SHARD()], [ExperimentalBlocks::AMETHYST_BLOCK()->asItem()])];
 	}
 }
