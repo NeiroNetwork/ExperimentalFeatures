2eba155001fed3a22fb3ac0f9ebe450210573ad7
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-28 21:28:00 +0900

    アメジストブロックにとりあえずメソッドを生やしておく

--- src/NeiroNetwork/ExperimentalFeatures/new/AmethystBlock.php
+++ src/NeiroNetwork/ExperimentalFeatures/new/AmethystBlock.php
@@ -1,1 +1,27 @@
 <?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\new;
+
+use NeiroNetwork\ExperimentalFeatures\new\interface\IBlock;
+use pocketmine\block\Block;
+
+class AmethystBlock implements IBlock{
+
+	public function internalId() : int{
+		// TODO: Implement internalId() method.
+	}
+
+	public function networkId() : int{
+		// TODO: Implement networkId() method.
+	}
+
+	public function name() : string{
+		// TODO: Implement name() method.
+	}
+
+	public function block() : Block{
+		// TODO: Implement block() method.
+	}
+}
