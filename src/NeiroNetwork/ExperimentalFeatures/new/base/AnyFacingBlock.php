ed132218d45f4bffae469dade29f58f8ea8cf359
Nerahikada <hkshirorom@yahoo.co.jp>
2021-09-29 18:04:26 +0900

    アメジストの塊を正しい向きで置けるようにした

--- src/NeiroNetwork/ExperimentalFeatures/new/base/AnyFacingBlock.php
+++ src/NeiroNetwork/ExperimentalFeatures/new/base/AnyFacingBlock.php
@@ -8,7 +8,6 @@ use pocketmine\block\Block;
 use pocketmine\block\utils\AnyFacingTrait;
 use pocketmine\block\utils\BlockDataSerializer;
 use pocketmine\item\Item;
-use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\BlockTransaction;
@@ -24,26 +23,8 @@ abstract class AnyFacingBlock extends Block{
 		$this->facing = BlockDataSerializer::readFacing($stateMeta);
 	}
 
-	/**
-	 * @see Barrel::place()
-	 */
 	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
-		if($player !== null){
-			if(abs($player->getPosition()->getX() - $this->position->getX()) < 2 && abs($player->getPosition()->getZ() - $this->position->getZ()) < 2){
-				$y = $player->getEyePos()->getY();
-
-				if($y - $this->position->getY() > 2){
-					$this->facing = Facing::UP;
-				}elseif($this->position->getY() - $y > 0){
-					$this->facing = Facing::DOWN;
-				}else{
-					$this->facing = Facing::opposite($player->getHorizontalFacing());
-				}
-			}else{
-				$this->facing = Facing::opposite($player->getHorizontalFacing());
-			}
-		}
-
+		$this->facing = $face;
 		return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
 	}
 }
