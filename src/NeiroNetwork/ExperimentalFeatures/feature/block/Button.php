260adaebadbb8ae4781a2163a7870dc7bdac35b6
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-13 00:32:25 +0900

    ボタンは押したときにバグるのでボタンを押せないようにしつつ楽な実装をする

---
+++ src/NeiroNetwork/ExperimentalFeatures/feature/block/Button.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\block;
+
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\world\BlockTransaction;
+
+class Button extends \pocketmine\block\Button{
+
+	protected function getActivationTime() : int{
+		return 0;
+	}
+
+	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		return true;
+	}
+
+	public function onNearbyBlockChange() : void{
+		if(!$this->getSide(Facing::opposite($this->facing))->isFullCube()){
+			$this->position->getWorld()->useBreakOn($this->position);
+		}
+	}
+
+	public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		if(!$this->getSide(Facing::opposite($face))->isFullCube()){
+			return false;
+		}
+		return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
+	}
+}
