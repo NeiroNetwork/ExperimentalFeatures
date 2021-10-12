2d83fcc535c8c9a1b407377a0c158467ed379c8e
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-12 23:40:09 +0900

    ボタン用のクラスを追加

---
+++ src/NeiroNetwork/ExperimentalFeatures/feature/block/WoodenButton.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\block;
+
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class WoodenButton extends \pocketmine\block\WoodenButton{
+
+	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		return true;
+	}
+}
