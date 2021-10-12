b70997449e33535075e87ffa827f93314f7f0964
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-12 23:34:20 +0900

    深層岩はTODO行き

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
@@ -5,40 +5,32 @@ declare(strict_types=1);
 namespace NeiroNetwork\ExperimentalFeatures\feature\v1_17;
 
 use NeiroNetwork\ExperimentalFeatures\feature\Feature;
-use NeiroNetwork\ExperimentalFeatures\feature\interface\HasRecipe;
-use NeiroNetwork\ExperimentalFeatures\feature\interface\IBlock;
-use NeiroNetwork\ExperimentalFeatures\feature\interface\Smeltable;
-use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalBlocks;
-use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalItems;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\HasRecipe;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
 use pocketmine\block\Opaque;
 use pocketmine\block\VanillaBlocks;
-use pocketmine\crafting\FurnaceRecipe;
 use pocketmine\item\Item;
 use pocketmine\item\ToolTier;
 
-class Deepslate extends Feature implements IBlock, Smeltable, HasRecipe{
+class Deepslate extends Feature implements IBlock, HasRecipe{
 
-	public function networkId() : int{
-		return -378;
-	}
-
-	public function name() : string{
+	public function stringId() : string{
 		return "deepslate";
 	}
 
 	public function block() : Block{
 		return new class(
-			$this->blockId(), "Deepslate",
+			$this->blockId(),
+			$this->displayName(),
 			new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 30.0)
 		) extends Opaque{
-
 			public function getDropsForCompatibleTool(Item $item) : array{
-				return [VanillaBlocks::COBBLESTONE()->asItem()];
+				// TODO: 深層岩の丸石 を追加する
+				return [];
 			}
-
 			public function isAffectedBySilkTouch() : bool{
 				return true;
 			}
@@ -46,8 +38,7 @@ class Deepslate extends Feature implements IBlock, Smeltable, HasRecipe{
 	}
 
 	public function recipe() : array{
-		return [new FurnaceRecipe(ExperimentalBlocks::Deepslate()->asItem(), VanillaBlocks::COBBLESTONE()->asItem())];
-
+		// TODO: レシピを追加する
+		return [];
 	}
-
 }
