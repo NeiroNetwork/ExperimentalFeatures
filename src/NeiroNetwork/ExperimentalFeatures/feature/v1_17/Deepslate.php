7878f5de2b6c01920fe8f0ef06aa70d20b72f62e
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-13 21:24:18 +0900

    インポートのクリーンアップ

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace NeiroNetwork\ExperimentalFeatures\feature\v1_17;
 
 use NeiroNetwork\ExperimentalFeatures\feature\Feature;
-use NeiroNetwork\ExperimentalFeatures\feature\interfaces\HasRecipe;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
 use pocketmine\block\Opaque;
-use pocketmine\block\VanillaBlocks;
 use pocketmine\item\Item;
 use pocketmine\item\ToolTier;
 
