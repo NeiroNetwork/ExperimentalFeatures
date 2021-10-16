a02d898e87e88c242be173ebed452bbd0b3fd282
gamingsay <kazuaugust08242@gmail.com>
2021-10-17 06:09:07 +0900

    修正:#9

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_16/PolishedBasalt.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_16/PolishedBasalt.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures\feature\v1_16;
 
+use NeiroNetwork\ExperimentalFeatures\feature\block\Pillar;
 use NeiroNetwork\ExperimentalFeatures\feature\Feature;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
-use pocketmine\block\Opaque;
 use pocketmine\item\ToolTier;
 
 class PolishedBasalt extends Feature implements IBlock{
@@ -19,7 +19,7 @@ class PolishedBasalt extends Feature implements IBlock{
 	}
 
 	public function block() : Block{
-		return new Opaque(
+		return new Pillar(
 			$this->blockId(),
 			$this->displayName(),
 			new BlockBreakInfo(1.25, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 21.0)
