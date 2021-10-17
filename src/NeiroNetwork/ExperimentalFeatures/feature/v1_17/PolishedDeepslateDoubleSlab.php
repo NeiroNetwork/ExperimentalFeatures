9b98d7c0aeaf62e44efada1dd86ef3ceece9ea12
gamingsay <kazuaugust08242@gmail.com>
2021-10-18 02:54:41 +0900

    追加:深層岩系

---
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/PolishedDeepslateDoubleSlab.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_17;
+
+use NeiroNetwork\ExperimentalFeatures\feature\block\DoubleSlab;
+use NeiroNetwork\ExperimentalFeatures\feature\Feature;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlockOnly;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockToolType;
+use pocketmine\item\ToolTier;
+
+class PolishedDeepslateDoubleSlab extends Feature implements IBlockOnly{
+
+	public function stringId() : string{
+		return "polished_deepslate_double_slab";
+	}
+
+	public function block() : Block{
+		return new DoubleSlab(
+			$this->blockId(),
+			$this->displayName(),
+			new BlockBreakInfo(3.5, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 30.0)
+		);
+	}
+}
