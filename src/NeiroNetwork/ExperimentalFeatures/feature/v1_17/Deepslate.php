cb1cb2bfd33810c19e0a6c39aabab4bf4b046b49
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-19 02:41:37 +0900

    深層岩は柱系のブロック

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/Deepslate.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures\feature\v1_17;
 
+use NeiroNetwork\ExperimentalFeatures\feature\block\Pillar;
 use NeiroNetwork\ExperimentalFeatures\feature\Feature;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalBlocks;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
-use pocketmine\block\Opaque;
 use pocketmine\item\Item;
 use pocketmine\item\ToolTier;
 
@@ -25,7 +25,7 @@ class Deepslate extends Feature implements IBlock{
 			$this->blockId(),
 			$this->displayName(),
 			new BlockBreakInfo(3.0, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 30.0)
-		) extends Opaque{
+		) extends Pillar{
 			public function getDropsForCompatibleTool(Item $item) : array{
 				return [ExperimentalBlocks::fromString("cobbled_deepslate")->asItem()];
 			}
