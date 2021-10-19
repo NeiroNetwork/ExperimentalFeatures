7a75c344dcef6ed7f113f1dc66a9d6ee53eab553
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-19 23:07:57 +0900

    深層ダイヤモンド鉱石の簡略化

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/DeepslateDiamondOre.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/DeepslateDiamondOre.php
@@ -9,10 +9,8 @@ use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
-use pocketmine\block\Opaque;
-use pocketmine\item\Item;
+use pocketmine\block\DiamondOre;
 use pocketmine\item\ToolTier;
-use pocketmine\item\VanillaItems;
 
 class DeepslateDiamondOre extends Feature implements IBlock{
 
@@ -21,19 +19,10 @@ class DeepslateDiamondOre extends Feature implements IBlock{
 	}
 
 	public function block() : Block{
-		return new class(
+		return new DiamondOre(
 			$this->blockId(),
 			$this->displayName(),
 			new BlockBreakInfo(4.5, BlockToolType::PICKAXE, ToolTier::IRON()->getHarvestLevel(), 30.0)
-		) extends Opaque{
-
-			public function getDropsForCompatibleTool(Item $item) : array{
-				return [VanillaItems::fromString("diamond")];
-			}
-
-			public function isAffectedBySilkTouch() : bool{
-				return true;
-			}
-		};
+		);
 	}
 }
