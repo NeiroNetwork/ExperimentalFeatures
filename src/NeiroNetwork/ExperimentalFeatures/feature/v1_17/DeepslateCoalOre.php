1b9ff0b4ce2bdc007a13356ba352a3bc591e9e1c
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-19 23:08:34 +0900

    深層石炭鉱石の簡略化

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/DeepslateCoalOre.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_17/DeepslateCoalOre.php
@@ -9,10 +9,8 @@ use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
-use pocketmine\block\Opaque;
-use pocketmine\item\Item;
+use pocketmine\block\CoalOre;
 use pocketmine\item\ToolTier;
-use pocketmine\item\VanillaItems;
 
 class DeepslateCoalOre extends Feature implements IBlock{
 
@@ -21,19 +19,10 @@ class DeepslateCoalOre extends Feature implements IBlock{
 	}
 
 	public function block() : Block{
-		return new class(
+		return new CoalOre(
 			$this->blockId(),
 			$this->displayName(),
 			new BlockBreakInfo(4.5, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 30.0)
-		) extends Opaque{
-
-			public function getDropsForCompatibleTool(Item $item) : array{
-				return [VanillaItems::fromString("coal")];
-			}
-
-			public function isAffectedBySilkTouch() : bool{
-				return true;
-			}
-		};
+		);
 	}
 }
