1695b69638d9fbd21a1995adc3342ff2e1526558
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-22 01:06:55 +0900

    たき火のメタデータを完全攻略しました(たぶん)

---
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_11/BaseCampfire.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_11;
+
+use pocketmine\block\Transparent;
+use pocketmine\block\utils\BlockDataSerializer;
+use pocketmine\block\utils\FacesOppositePlacingPlayerTrait;
+use pocketmine\block\utils\NormalHorizontalFacingInMetadataTrait;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class BaseCampfire extends Transparent{
+	use FacesOppositePlacingPlayerTrait;
+	use NormalHorizontalFacingInMetadataTrait;
+
+	protected bool $extinguished = true;
+
+	protected function writeStateToMeta() : int{
+		return BlockDataSerializer::writeLegacyHorizontalFacing($this->facing) | ($this->extinguished ? 0b100 : 0);
+	}
+
+	public function readStateFromData(int $id, int $stateMeta) : void{
+		$this->facing = BlockDataSerializer::readLegacyHorizontalFacing($stateMeta & 0b011);
+		$this->extinguished = ($stateMeta & 0b100) !== 0;
+	}
+
+	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
+		$this->extinguished = !$this->extinguished;
+		$this->position->getWorld()->setBlock($this->position, $this);
+		return true;
+	}
+}
--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_11/Campfire.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_11/Campfire.php
@@ -9,7 +9,6 @@ use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
-use pocketmine\block\Transparent;
 use pocketmine\item\ToolTier;
 
 class Campfire extends Feature implements IBlock{
@@ -19,7 +18,7 @@ class Campfire extends Feature implements IBlock{
 	}
 
 	public function block() : Block{
-		return new Transparent(
+		return new BaseCampfire(
 			$this->blockId(),
 			$this->displayName(),
 			new BlockBreakInfo(2.0, BlockToolType::AXE, ToolTier::WOOD()->getHarvestLevel())
