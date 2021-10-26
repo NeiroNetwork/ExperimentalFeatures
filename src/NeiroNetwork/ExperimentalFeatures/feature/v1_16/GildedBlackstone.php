9162aa439ebd3e063f2ca0e05756c774480ba79d
gamingsay <kazuaugust08242@gmail.com>
2021-10-27 00:11:21 +0900

    Update GildedBlackstone.php

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_16/GildedBlackstone.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_16/GildedBlackstone.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures\feature\v1_16;
 
-use NeiroNetwork\ExperimentalFeatures\feature\block\Pillar;
 use NeiroNetwork\ExperimentalFeatures\feature\Feature;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
-use NeiroNetwork\ExperimentalFeatures\registry\ExperimentalBlocks;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
@@ -21,6 +19,7 @@ class GildedBlackstone extends Feature implements IBlock{
 	public function stringId() : string{
 		return "gilded_blackstone";
 	}
+
 	public function block() : Block{
 		return new class(
 			$this->blockId(),
@@ -28,9 +27,14 @@ class GildedBlackstone extends Feature implements IBlock{
 			new BlockBreakInfo(1.5, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 30.0)
 		) extends Opaque{
 			public function getDropsForCompatibleTool(Item $item) : array{
-				return
-				 [VanillaItems::GOLD_NUGGET()->setCount(mt_rand(2,5))];
+				if(mt_rand(1, 10) === 1){
+					return
+						[VanillaItems::GOLD_NUGGET()->setCount(mt_rand(2, 5))];
+				}else{
+					return [];
+				}
 			}
+
 			public function isAffectedBySilkTouch() : bool{
 				return true;
 			}
