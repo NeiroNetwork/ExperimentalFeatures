bf8f61549443640ca75acd0f4004e5ed384c493b
Nerahikada <hkshirorom@yahoo.co.jp>
2021-10-29 10:51:57 +0900

    クライアントに水が残るバグを修正, close #32

--- src/NeiroNetwork/ExperimentalFeatures/EventListener.php
+++ src/NeiroNetwork/ExperimentalFeatures/EventListener.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\ExperimentalFeatures;
 
+use NeiroNetwork\ExperimentalFeatures\override\expert\item\WaterBucketFixer;
 use pocketmine\block\Fire;
 use pocketmine\block\VanillaBlocks;
+use pocketmine\block\Water;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\world\sound\FireExtinguishSound;
@@ -14,6 +17,7 @@ class EventListener implements Listener{
 
 	/**
 	 * FIXME: どうしようも出来なかったのでイベントで魂の炎を消すようにする
+	 *
 	 * @priority MONITOR
 	 */
 	public function onInteract(PlayerInteractEvent $event) : void{
@@ -22,6 +26,18 @@ class EventListener implements Listener{
 			$world = $block->getPosition()->getWorld();
 			$world->setBlock($block->getPosition(), VanillaBlocks::AIR());
 			$world->addSound($block->getPosition()->add(0.5, 0.5, 0.5), new FireExtinguishSound());
+		}
+	}
+
+	/**
+	 * クライアントに水が残ってしまうバグ(?)を修正する
+	 *
+	 * @priority MONITOR
+	 */
+	public function fixClientSideWaterRemains(BlockPlaceEvent $event) : void{
+		$replaced = $event->getBlockReplaced();
+		if($replaced instanceof Water && $replaced->getDecay() === 0){
+			WaterBucketFixer::removeWaterFrom($event->getPlayer(), $replaced->getPosition());
 		}
 	}
 }
--- src/NeiroNetwork/ExperimentalFeatures/override/expert/item/WaterBucketFixer.php
+++ src/NeiroNetwork/ExperimentalFeatures/override/expert/item/WaterBucketFixer.php
@@ -12,24 +12,27 @@ use pocketmine\item\LiquidBucket;
 use pocketmine\item\VanillaItems;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\convert\RuntimeBlockMapping;
+use pocketmine\network\mcpe\protocol\types\BlockPosition;
 use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
 use pocketmine\player\Player;
 
 class WaterBucketFixer extends ItemOverrideExpert{
 
+	public static function removeWaterFrom(Player $player, Vector3 $pos) : void{
+		/** @noinspection PhpInternalEntityUsedInspection */
+		$pk = UpdateBlockPacket::create(BlockPosition::fromVector3($pos),
+			RuntimeBlockMapping::getInstance()->toRuntimeId(VanillaBlocks::AIR()->getFullId()),
+			UpdateBlockPacket::FLAG_NETWORK,
+			UpdateBlockPacket::DATA_LAYER_LIQUID);
+		$player->getNetworkSession()->sendDataPacket($pk);
+	}
+
 	protected function water_bucket() : Item{
 		$i = VanillaItems::WATER_BUCKET();
 		return new class($this->toIdentifier($i), $i->getName(), $i->getLiquid()) extends LiquidBucket{
 			public function onInteractBlock(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector) : ItemUseResult{
-				$removeWater = function(Vector3 $pos) use ($player) : void{
-					/** @noinspection PhpInternalEntityUsedInspection */
-					$runtimeId = RuntimeBlockMapping::getInstance()->toRuntimeId(VanillaBlocks::AIR()->getFullId());
-					$pk = UpdateBlockPacket::create($pos->x, $pos->y, $pos->z, $runtimeId);
-					$pk->dataLayerId = UpdateBlockPacket::DATA_LAYER_LIQUID;
-					$player->getNetworkSession()->sendDataPacket($pk);
-				};
-				$removeWater($blockReplace->getPosition());
-				$removeWater($blockClicked->getPosition());
+				WaterBucketFixer::removeWaterFrom($player, $blockReplace->getPosition());
+				WaterBucketFixer::removeWaterFrom($player, $blockClicked->getPosition());
 				return parent::onInteractBlock($player, $blockReplace, $blockClicked, $face, $clickVector);
 			}
 		};
