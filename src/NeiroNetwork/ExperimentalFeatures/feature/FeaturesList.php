eb23657b6041436337b74ad20473d9cae0d1370b
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-01 02:47:41 +0900

    FeaturesList: インポートのクリーンアップ

--- src/NeiroNetwork/ExperimentalFeatures/feature/FeaturesList.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/FeaturesList.php
@@ -78,9 +78,8 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_17\AmethystShard;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\Azalea;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\AzaleaLeaves;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\AzaleaLeavesFlowered;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\FloweringAzalea;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\BuddingAmethyst;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\Calcite;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ChiseledDeepslate;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CobbledDeepslate;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CobbledDeepslateDoubleSlab;
@@ -93,7 +92,6 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CopperOre;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CrackedDeepslateBricks;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CrackedDeepslateTiles;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CutCopper;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DoubleCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\CutCopperStairs;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\Deepslate;
@@ -115,11 +113,13 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DeepslateTiles;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DeepslateTileSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DeepslateTileStairs;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DeepslateTileWall;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\DoubleCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedCopper;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedCutCopper;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedCutCopperStairs;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedDoubleCutCopperSlab;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\ExposedCutCopperSlab;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\FloweringAzalea;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\GlowInkSac;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\InfestedDeepslate;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\LargeAmethystBud;
@@ -127,6 +127,7 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_17\LitDeepslateRedstoneOre;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\MediumAmethystBud;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedCopper;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedCutCopper;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedCutCopperStairs;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\OxidizedDoubleCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\PolishedDeepslate;
@@ -142,14 +143,12 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_17\RawIron;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\RawIronBlock;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\SmallAmethystBud;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\TintedGlass;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\Calcite;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\Tuff;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredCopper;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredCutCopper;
-use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredDoubleCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredCutCopperSlab;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredCutCopperStairs;
-use Symfony\Component\Translation\Writer\TranslationWriterInterface;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_17\WeatheredDoubleCutCopperSlab;
 
 final class FeaturesList{
 
@@ -294,7 +293,7 @@ final class FeaturesList{
 		Azalea::class,
 		AzaleaLeaves::class,
 		AzaleaLeavesFlowered::class,
-    GildedBlackstone::class,
+		GildedBlackstone::class,
 		CrackedPolishedBlackstoneBricks::class,
 		ChiseledPolishedBlackstone::class,
 		PolishedBlackstoneBricks::class,
