42912b302685d8343e2aadc7c029d88834829369
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-06 21:12:15 +0900

    HoneycombBlock: 不要なインポートの削除

--- src/NeiroNetwork/ExperimentalFeatures/feature/v1_14/HoneycombBlock.php
+++ src/NeiroNetwork/ExperimentalFeatures/feature/v1_14/HoneycombBlock.php
@@ -8,9 +8,7 @@ use NeiroNetwork\ExperimentalFeatures\feature\Feature;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
-use pocketmine\block\BlockToolType;
 use pocketmine\block\Opaque;
-use pocketmine\item\ToolTier;
 
 class HoneycombBlock extends Feature implements IBlock{
 
