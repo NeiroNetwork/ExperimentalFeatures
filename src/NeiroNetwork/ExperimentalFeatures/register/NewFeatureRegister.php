32432545f965ecf97f8115584e0be2b8fed57ce8
Nerahikada <hkshirorom@yahoo.co.jp>
2021-11-09 20:32:13 +0900

    ItemBlockは256未満というクソ仕様を絶対抹殺するマン
    このクソ仕様はマインクラフトが実装した感があります

--- src/NeiroNetwork/ExperimentalFeatures/register/NewFeatureRegister.php
+++ src/NeiroNetwork/ExperimentalFeatures/register/NewFeatureRegister.php
@@ -15,6 +15,7 @@ use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\item\ItemBlock;
 use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIdentifier;
 use pocketmine\item\LegacyStringToItemParser;
 use pocketmine\item\StringToItemParser;
 
@@ -30,21 +31,30 @@ class NewFeatureRegister{
 			BlockFactory::getInstance()->register($feature->block());
 			ExperimentalBlocks::register($feature->stringId(), BlockFactory::getInstance()->get($feature->blockId()->getBlockId(), 0));
 			StringToItemParser::getInstance()->registerBlock($feature->stringId(), \Closure::fromCallable([ExperimentalBlocks::class, "fromString"]));
-			LegacyStringToItemParser::getInstance()->addMapping($feature->stringId(), $feature->itemId()->getId());
-			LegacyStringToItemParser::getInstance()->addMapping((string) $feature->itemId()->getId(), $feature->itemId()->getId());
-			$hacks->blameLegacyStringToItemParser->registerBlock($feature);
 			$blocks = $hacks->runtimeBlockMapping->hack($feature->fullStringId(), ExperimentalBlocks::fromString($feature->stringId()));
 			array_map(fn(Block $block) => BlockFactory::getInstance()->register($block), $blocks);
 
+			LegacyStringToItemParser::getInstance()->addMapping($feature->stringId(), $feature->itemId()->getId());
+			LegacyStringToItemParser::getInstance()->addMapping((string) $feature->itemId()->getId(), $feature->itemId()->getId());
+			$hacks->blameLegacyStringToItemParser->registerBlock($feature);
+
 			if(!$feature->isRegisteredPmmp()){
-				ItemFactory::getInstance()->register(new ItemBlock($feature->itemId(), ExperimentalBlocks::fromString($feature->stringId())));
 				$hacks->legacyBlockIdToStringIdMap->hack($feature->fullStringId(), $feature->blockId()->getBlockId());
 				$hacks->legacyItemIdToStringIdMap->hack($feature->fullStringId(), $feature->itemId()->getId());
 				$hacks->itemTranslator->hack($feature->itemId()->getId(), $feature->runtimeId());
 			}
 
+			// ItemBlockのIDは256未満 というルールを破ったアイテムが一部存在するため、そういう場合はItemBlockを登録してあげる
 			if(ItemFactory::getInstance()->get($feature->itemId()->getId(), $feature->itemId()->getMeta())->getName() === "Unknown"){
 				ItemFactory::getInstance()->register(new ItemBlock($feature->itemId(), ExperimentalBlocks::fromString($feature->stringId())));
+			}
+
+			// (今の仕様だと)PocketMine-MPに登録されていないアイテムが256未満になることはないので、ItemBlockを登録してあげる必要がある
+			if(!$feature->isRegisteredPmmp()){
+				ItemFactory::getInstance()->register(new ItemBlock($feature->itemId(), ExperimentalBlocks::fromString($feature->stringId())));
+				foreach($blocks as $block){
+					ItemFactory::getInstance()->register(new ItemBlock(new ItemIdentifier($feature->itemId()->getId(), $block->getMeta()), $block));
+				}
 			}
 		}
 
