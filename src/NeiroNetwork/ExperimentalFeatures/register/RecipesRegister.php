2b3b728ed3bdd1304b6e07a18c7209cdded7a86a
Nerahikada <hkshirorom@yahoo.co.jp>
2022-05-27 00:48:12 +0900

    1.18のレシピを追加(?)

--- src/NeiroNetwork/ExperimentalFeatures/register/RecipesRegister.php
+++ src/NeiroNetwork/ExperimentalFeatures/register/RecipesRegister.php
@@ -23,6 +23,7 @@ class RecipesRegister{
 
 	public static function registerAll(PluginBase $plugin) : void{
 		self::register($plugin->getResource("recipes_1.17.json"));
+		self::register($plugin->getResource("recipes_1.18.json"));
 		self::fixRecipes();
 	}
 
