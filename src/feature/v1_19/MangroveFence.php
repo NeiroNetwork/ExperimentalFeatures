c3c616b9909f6af4075b82cbae43ca409571677f
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-10 23:22:23 +0900

    マングローブのフェンス を追加

---
+++ src/feature/v1_19/MangroveFence.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_19;
+
+use NeiroNetwork\ExperimentalFeatures\feature\Feature;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockToolType;
+use pocketmine\block\Fence;
+
+class MangroveFence extends Feature implements IBlock{
+
+	public function stringId() : string{
+		return "mangrove_fence";
+	}
+
+	public function block() : Block{
+		return new Fence(
+			$this->blockId(),
+			$this->displayName(),
+			new BlockBreakInfo(2.0, BlockToolType::AXE, 0, 15.0)
+		);
+	}
+}
