f1df0a6590b56295c0e9a1c70dd0c7c383eb14ad
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-10 23:54:09 +0900

    InternalIdCounter: pmmpとIDの衝突を避けるために、IDの振り分けを810からに変更

--- src/feature/InternalIdCounter.php
+++ src/feature/InternalIdCounter.php
@@ -6,7 +6,12 @@ namespace NeiroNetwork\ExperimentalFeatures\feature;
 
 final class InternalIdCounter{
 
-	private static int $count = 600;
+	/**
+	 * block_id_map.json, item_id_map.json を参照
+	 * ブロック: 0 ~ 559
+	 * アイテム: -304 ~ 801
+	 */
+	private static int $count = 810;
 
 	public static function next() : int{
 		return self::$count++;
