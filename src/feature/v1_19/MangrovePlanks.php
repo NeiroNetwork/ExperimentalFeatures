80f9cefbc004a75cd87cda8c105ca441761b7678
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-10 21:30:30 +0900

    マングローブの板材 を追加

---
+++ src/feature/v1_19/MangrovePlanks.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_19;
+
+use NeiroNetwork\ExperimentalFeatures\feature\Feature;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockToolType;
+use pocketmine\block\Opaque;
+
+class MangrovePlanks extends Feature implements IBlock{
+
+	public function stringId() : string{
+		return "mangrove_planks";
+	}
+
+	public function block() : Block{
+		return new Opaque(
+			$this->blockId(),
+			$this->displayName(),
+			new BlockBreakInfo(2.0, BlockToolType::AXE, 0, 15.0)
+		);
+	}
+}
