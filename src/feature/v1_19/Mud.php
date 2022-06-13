8c0d6f408460310afe25645b444a3e129587fc86
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-13 16:36:25 +0900

    泥を追加

---
+++ src/feature/v1_19/Mud.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_19;
+
+use NeiroNetwork\ExperimentalFeatures\feature\Feature;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockToolType;
+use pocketmine\block\Opaque;
+
+class Mud extends Feature implements IBlock{
+
+	public function stringId() : string{
+		return "mud";
+	}
+
+	public function block() : Block{
+		return new Opaque(
+			$this->blockId(),
+			$this->displayName(),
+			new BlockBreakInfo(0.5, BlockToolType::SHOVEL)
+		);
+	}
+}
