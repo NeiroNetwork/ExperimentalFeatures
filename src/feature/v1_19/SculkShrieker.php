0280d9eeae7eb35cc7bba97c3f4f201eeae1e472
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-21 00:01:08 +0900

    SculkShrieker: ブロックのアクティベーションなどを実装

--- src/feature/v1_19/SculkShrieker.php
+++ src/feature/v1_19/SculkShrieker.php
@@ -11,6 +11,7 @@ use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockToolType;
 use pocketmine\block\Opaque;
 use pocketmine\block\utils\SupportType;
+use pocketmine\entity\Entity;
 use pocketmine\item\Item;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Facing;
@@ -27,11 +28,49 @@ class SculkShrieker extends Feature implements IBlock{
 			$this->displayName(),
 			new BlockBreakInfo(3.0, BlockToolType::HOE)
 		) extends Opaque{
-			public function getDropsForCompatibleTool(Item $item) : array{ return []; }
+			private bool $active = false;
+			private bool $canSummon = false;
 
-			protected function getXpDropAmount() : int{ return 5; }
+			public function readStateFromData(int $id, int $stateMeta) : void{
+				$this->active = ($stateMeta & 0b10) !== 0;
+				$this->canSummon = ($stateMeta & 0b01) !== 0;
+			}
 
-			public function isAffectedBySilkTouch() : bool{ return true; }
+			protected function writeStateToMeta() : int{
+				return ($this->active ? 0b10 : 0) | ($this->canSummon ? 0b01 : 0);
+			}
+
+			public function getStateBitmask() : int{
+				return 0b11;
+			}
+
+			public function getActive() : bool{
+				return $this->active;
+			}
+
+			public function setActive(bool $active) : void{
+				$this->active = $active;
+			}
+
+			public function getCanSummon() : bool{
+				return $this->canSummon;
+			}
+
+			public function setCanSummon(bool $canSummon) : void{
+				$this->canSummon = $canSummon;
+			}
+
+			public function getDropsForCompatibleTool(Item $item) : array{
+				return [];
+			}
+
+			protected function getXpDropAmount() : int{
+				return 5;
+			}
+
+			public function isAffectedBySilkTouch() : bool{
+				return true;
+			}
 
 			protected function recalculateCollisionBoxes() : array{
 				return [AxisAlignedBB::one()->trim(Facing::UP, 8 / 16)];
@@ -39,6 +78,23 @@ class SculkShrieker extends Feature implements IBlock{
 
 			public function getSupportType(int $facing) : SupportType{
 				return $facing === Facing::DOWN ? SupportType::FULL() : SupportType::NONE();
+			}
+
+			public function onEntityLand(Entity $entity) : ?float{
+				if(!$this->active){
+					$this->active = true;
+					$this->position->world->setBlock($this->position, $this);
+					$this->position->world->scheduleDelayedBlockUpdate($this->position, 90);
+					// TODO: play sound, add particle
+				}
+				return parent::onEntityLand($entity);
+			}
+
+			public function onScheduledUpdate() : void{
+				if($this->active){
+					$this->active = false;
+					$this->position->world->setBlock($this->position, $this);
+				}
 			}
 		};
 	}
