7c0373ca520fbdf403fa4bfca93d62956e715b9f
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-20 10:40:28 +0900

    (開花した)ツツジ: 当たり判定を修正
    NeiroNetwork/PeekAntiCheatLite#44 に関連

--- src/feature/v1_17/Azalea.php
+++ src/feature/v1_17/Azalea.php
@@ -9,6 +9,7 @@ use NeiroNetwork\ExperimentalFeatures\feature\Feature;
 use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
+use pocketmine\math\AxisAlignedBB;
 
 class Azalea extends Feature implements IBlock{
 
@@ -17,6 +18,10 @@ class Azalea extends Feature implements IBlock{
 	}
 
 	public function block() : Block{
-		return new Sapling($this->blockId(), $this->displayName(), BlockBreakInfo::instant());
+		return new class($this->blockId(), $this->displayName(), BlockBreakInfo::instant()) extends Sapling{
+			protected function recalculateCollisionBoxes() : array{
+				return [AxisAlignedBB::one()];
+			}
+		};
 	}
 }
