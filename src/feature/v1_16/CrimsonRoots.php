77011cc98b64ee575c4968f913e48bef86a4286b
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-23 11:23:51 +0900

    真紅の根、歪んだ根 を追加

---
+++ src/feature/v1_16/CrimsonRoots.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\ExperimentalFeatures\feature\v1_16;
+
+use NeiroNetwork\ExperimentalFeatures\feature\Feature;
+use NeiroNetwork\ExperimentalFeatures\feature\interfaces\IBlock;
+use NeiroNetwork\ExperimentalFeatures\feature\v1_16\block\Roots;
+use pocketmine\block\Block;
+use pocketmine\block\BlockBreakInfo;
+
+class CrimsonRoots extends Feature implements IBlock{
+
+	public function stringId() : string{
+		return "crimson_roots";
+	}
+
+	public function block() : Block{
+		return new Roots($this->blockId(), $this->displayName(), BlockBreakInfo::instant());
+	}
+}
